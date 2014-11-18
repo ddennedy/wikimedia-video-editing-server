3644748c292ed106618c6b699d5e5b3e4461fab8
Dan Dennedy <dan@dennedy.org>
2014-11-17 20:38:12 -0800

    Add header with menu and footer templates and a main page.

---
+++ application/controllers/Main.php
@@ -0,0 +1,36 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * Wikimedia Video Editing Server
+ * Copyright (C) 2014 Dan R. Dennedy <dan@dennedy.org>
+ * Copyright (C) 2014 CDC Leuphana University Lueneburg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Main extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data = array();
+        $data = array_merge($data, $this->lang->language);
+        $this->load->view('templates/header', $data);
+        $this->load->view('main/index', $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
