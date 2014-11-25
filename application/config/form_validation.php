18771ad8b9cae3c5b26a27593595ec5ff76bc0e6
Dan Dennedy <dan@dennedy.org>
2014-11-25 05:38:17 +0000

    Use jquery UI to style form validation errors.

---
+++ application/config/form_validation.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * Wikimedia Video Editing Server
+ * Copyright (C) 2014 Dan R. Dennedy <dan@dennedy.org>
+ * Copyright (C) 2014 CDC Leuphana University Lueneburg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$config['error_prefix'] = '<div class="ui-widget">
+	<div class="ui-state-error ui-corner-all" style="padding: 0 .7em;">
+		<p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span>';
+$config['error_suffix'] = '</p></div></div><br>';
